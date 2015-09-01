a3e4978c8f52921c9369d2054569912f90362cc2
netpoe <yo@soygus.com>
2015-09-01 15:21:37 +0000

    sql test content corrections

--- .guides/sqltests/sql-1-5.sh
+++ .guides/sqltests/sql-1-5.sh
@@ -14,19 +14,19 @@ function test_command {
         expect_command "SHOW TABLES;" "Display the available tables of the directory database"
         ;;
       3 )
-        expect_command "SELECT * FROM company_profiles;" "Query all the records of the company_profiles table"
+        expect_command "SELECT * FROM company_profiles;" "Query all the records from the company_profiles table"
         ;;
       4 )
         expect_command "SELECT * FROM company_profiles LIMIT 5;" "Select all the records from the company_profiles table and limit them to 5 rows only"
         ;;
       5 )
-        expect_command "SELECT id,company_name FROM company_profiles ORDER BY id DESC;" "Select only the id and the company_name columns of the company_profiles table and sort them by id in descending order"
+        expect_command "SELECT id,company_name FROM company_profiles ORDER BY id DESC;" "Select only the id and the company_name columns from the company_profiles table and sort them by id in descending order"
         ;;
       6 )
         expect_command "SELECT * FROM company_profiles WHERE company_name = 'Lorem Ipsum Institute' OR company_name = 'Et LLP';" "Select all the company_profiles records where company_name is equal to Lorem Ipsum Institute or Et LLP;"
         ;;
       7 )
-        expect_command "SELECT id,phone_num FROM company_profiles WHERE id BETWEEN 40 AND 60;" "Finally, select only the id and phone_num attributes of the company_profiles table and get the records which id is between 40 and 60"
+        expect_command "SELECT id,phone_num FROM company_profiles WHERE id BETWEEN 40 AND 60;" "Finally, select only the id and phone_num attributes of the company_profiles table and get the records where the id is between 40 and 60"
         ;;
 		esac
 	else		
